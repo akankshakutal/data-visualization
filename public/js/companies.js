0a893ee166400e74df78740469f1361e78bfc473
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2020-02-12 18:28:16 +0530

    Company Data vizualization

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,26 +1,26 @@
-const drawBuildings = buildings => {
+const drawBuildings = companies => {
   const container = d3.select("#chart-area");
-  const maxHeight = _.maxBy(buildings, building => building.height);
-  const chartSize = { width: 600, height: 400 };
-  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+  const maxHeight = _.maxBy(companies, company => +company.CMP);
+  const chartSize = { width: 800, height: 600 };
+  const margin = { left: 100, right: 100, top: 10, bottom: 150 };
   const width = chartSize.width - (margin.left + margin.right);
   const height = chartSize.height - (margin.top + margin.bottom);
 
   const x = d3
     .scaleBand()
-    .domain(_.map(buildings, "name"))
+    .domain(_.map(companies, "Name"))
     .range([0, width])
     .padding(0.3);
 
   const y = d3
     .scaleLinear()
-    .domain([0, maxHeight.height])
+    .domain([0, maxHeight.CMP])
     .range([height, 0]);
 
   const y_axis = d3
     .axisLeft(y)
-    .tickFormat(d => d + "m")
-    .ticks(3);
+    .tickFormat(d => "₹" + d)
+    .ticks(10);
 
   const x_axis = d3.axisBottom(x);
 
@@ -56,7 +56,7 @@ const drawBuildings = buildings => {
     .attr("x", width / 2)
     .attr("y", height + 140)
     .attr("class", "axis-label")
-    .text("Tall Building");
+    .text("Companies");
 
   group
     .append("text")
@@ -64,21 +64,21 @@ const drawBuildings = buildings => {
     .attr("transform", `rotate(-90)`)
     .attr("x", -height / 2)
     .attr("y", -60)
-    .text("Height(m)");
+    .text("CMP(RS)");
 
-  const rectangle = group.selectAll("rect").data(buildings);
+  const rectangle = group.selectAll("rect").data(companies);
 
   const newRects = rectangle
     .enter()
     .append("rect")
-    .attr("y", b => y(b.height))
-    .attr("x", b => x(b.name))
-    .attr("width", 40)
-    .attr("height", b => y(0) - y(b.height));
+    .attr("y", b => y(b.CMP))
+    .attr("x", b => x(b.Name))
+    .attr("width", 60)
+    .attr("height", b => y(0) - y(b.CMP));
 };
 
 const main = () => {
-  d3.json("data/buildings.json").then(drawBuildings);
+  d3.csv("data/companies.csv").then(drawBuildings);
 };
 
 window.onload = main;
